0d802bef1efe7fe515f01bdd825110f169700c0c
Nikunj Mistry <nikunjmistry1000@gmail.com>
2022-07-05 17:19:42 +0530

    Update power problem using modulo arithmetic

--- Recursion/basic recursion(power)/recursion.cpp
+++ Recursion/basic recursion(power)/recursion.cpp
@@ -16,7 +16,7 @@ int main()
 
 //***********************************
 //Power of a raise to b using recursion
-#include<bits/stdc++.h>
+/*#include<bits/stdc++.h>
 using namespace std;
 
 int power(int a,int b){
@@ -29,4 +29,33 @@ return a*power(a,b-1);
 int main()
 {
  cout<< power(3,4);
+}*/
+
+//*********************
+//using Modulo arithmetic
+/*
+Using modulo property
+(a+b)%n= a%n+b%n
+(a*b)%n=(a%n*b%n)%n
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+long int power(long int a,long int b,int n){
+long int res=1;
+while(b>0){
+    if((b&1)!=0){
+        res=(res*a%n)%n;
+    }
+    a=(a%n*a%n);
+    b=b>>1;
 }
+return res;
+}
+int main()
+{
+  cout<<power(24,5,1000000007);
+
+}
+s
